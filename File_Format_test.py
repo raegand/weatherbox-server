2e23b7f56af932135a1946b0d42e3ae26ae4e03a
Raegan Diana <zayev.rae@gmail.com>
2015-11-24 13:33:35 -1000

    Added comments and added format test for sanity

--- File_Format_test.py
+++ File_Format_test.py
@@ -1,9 +1,11 @@
-def test(file):
-
+def pack_format(file):
+    #Open file to read
     file = open(file, 'r')
+    #Initialize packing format to empty string
     struct_fmt = ''
+    #Loop through each line in passed file
     for line in file:
-        print line
+        #Add correct format variable depending on structure from passed file
         if line == "int8\n" or line == "int8":
             struct_fmt = struct_fmt + 'b'
 
@@ -21,15 +23,19 @@ def test(file):
 
         if line == "uint32\n" or line == "uint32":
             struct_fmt = struct_fmt + 'I'
-
+    #Close file
     file.close()
+    #Return packing format to test
     return struct_fmt
 
+#Test call using schema3 from github struct
+pack_format = pack_format("schema3.txt")
 
-pack_format = test("schema3.txt")
-print pack_format
-
+#x is hard coded format from original Data-Translator file
 x = 'H'+'H'+'B'+'I'+'B'+'H'+'H'+'I'+'h'+'H'+'H'
-
+#Print hard coded format and generated format for sanity check
+print pack_format
+print x
+#Compare both strings to be sure they are the same
 if x == pack_format:
     print True
