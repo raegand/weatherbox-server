7a024c23661e80c5734fa3f5a4c567a02ceb4878
Raegan Diana <zayev.rae@gmail.com>
2015-11-24 13:24:59 -1000

    File_Format creates correct packing format according to schema3 test (tested against hard coded format)

--- File_Format_test.py
+++ File_Format_test.py
@@ -1,16 +1,35 @@
 def test(file):
+
     file = open(file, 'r')
-    struct_fmt = 'a'
-    x = 0
+    struct_fmt = ''
     for line in file:
         print line
         if line == "int8\n" or line == "int8":
             struct_fmt = struct_fmt + 'b'
-            print struct_fmt + '\n'
+
         if line == "uint8\n" or line == "uint8":
             struct_fmt = struct_fmt + 'B'
-            print struct_fmt + '\n'
+
+        if line == "int16\n" or line == "int16":
+            struct_fmt = struct_fmt + 'h'
+
+        if line == "uint16\n" or line == "uint16":
+            struct_fmt = struct_fmt + 'H'
+
+        if line == "int32\n" or line == "int32":
+            struct_fmt = struct_fmt + 'i'
+
+        if line == "uint32\n" or line == "uint32":
+            struct_fmt = struct_fmt + 'I'
+
     file.close()
+    return struct_fmt
 
 
-test("test.txt")
+pack_format = test("schema3.txt")
+print pack_format
+
+x = 'H'+'H'+'B'+'I'+'B'+'H'+'H'+'I'+'h'+'H'+'H'
+
+if x == pack_format:
+    print True
