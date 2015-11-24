ef53dd0bbca5078a7ee03130ba0da0fffba14337
Raegan Diana <zayev.rae@gmail.com>
2015-11-24 12:04:17 -1000

    Test file read/format generation works better than previous code

--- File_Format_test.py
+++ File_Format_test.py
@@ -1,15 +1,12 @@
-def format(file):
-    format_file = open(file, "r")
+def test(file):
+    file = open(file, 'r')
     struct_fmt = 'a'
-    for line in format_file:
-        if line == "int8":
+    x = 0
+    for line in file:
+        print line
+        if line == 'int8\n' or 'int8':
             struct_fmt = struct_fmt + 'b'
-    format_file.close()
+    file.close()
     print struct_fmt
 
-
-file = open("test.txt", "r")
-print file.readline()
-file.close()
-
-format("test.txt")
+test("test.txt")
