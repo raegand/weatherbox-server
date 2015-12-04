63531142909711dabb2cead7ba5f238148cc109c
Raegan Diana <zayev.rae@gmail.com>
2015-12-03 18:30:22 -1000

    Merged Packing/Unpacking code and Packing Format files into one code -Comments not yet added properly

--- Data_Packing_Unpacking.py
+++ Data_Packing_Unpacking.py
@@ -1,11 +1,18 @@
+#Import struct library to use the packing and unpacking function
 import struct
+#Import binascii just for printing packed format in a nicer format during testing
 import binascii
 
+#Fuction created to pack passed data
 def pack(file, schema, address, overflow_num, uptime_ms, n, batt_mv, panel_mv, bmp085_press_pa, bmp085_temp_decic, humidity_centi_pct, apogee_2_m2):
+    #Open file for read only to avoid accidental writing
     file = open(file, 'r')
+    #Initialize packing format to empty string so more string elements can be added to it
     struct_fmt = ''
+    #Loop through each line in the format file to be sure everything is packed properly
     for line in file:
         #Add correct format variable depending on structure from passed file
+        #Python packing formatting can be found on https://docs.python.org/2/library/struct.html
         if line == "int8\n" or line == "int8":
             struct_fmt = struct_fmt + 'b'
 
@@ -23,17 +30,23 @@ def pack(file, schema, address, overflow_num, uptime_ms, n, batt_mv, panel_mv, b
 
         if line == "uint32\n" or line == "uint32":
             struct_fmt = struct_fmt + 'I'
-    #Close file
+    #Close the file to avoid errors
     file.close()
     #struct_fmt = 'H'+'H'+'B'+'I'+'B'+'H'+'H'+'I'+'h'+'H'+'H'
+    #Pack the passed data using the packing function in struct and the struct_fmt (format)
     packed_data = struct.pack(struct_fmt, schema, address, overflow_num, uptime_ms, n, batt_mv, panel_mv, bmp085_press_pa, bmp085_temp_decic, humidity_centi_pct, apogee_2_m2)
+    #Return packed data for tests
     return packed_data
 
 def unpack(file, packed_data):
+    #Open file for read only to avoid accidental writing
     file = open(file, 'r')
+    #Initialize packing format to empty string so more string elements can be added to it
     struct_fmt = ''
+    #Loop through each line in the format file to be sure everything is packed properly
     for line in file:
-        #Add format variable depending on structure from passed file
+        #Add correct format variable depending on structure from passed file
+        #Python packing formatting can be found on https://docs.python.org/2/library/struct.html
         if line == "int8\n" or line == "int8":
             struct_fmt = struct_fmt + 'b'
 
@@ -51,12 +64,26 @@ def unpack(file, packed_data):
 
         if line == "uint32\n" or line == "uint32":
             struct_fmt = struct_fmt + 'I'
-    #Close file
+    #Close the file to avoid errors
     file.close()
+    #Hard coded packing format commented out to properly run the format generation code
    # struct_fmt = 'H'+'H'+'B'+'I'+'B'+'H'+'H'+'I'+'h'+'H'+'H'
+    #Unpack the passed data using the same packing format and the unpacking function in the struct library
+        #Note:  If a different format is used to unpack the data, you won't get the original data you started with
     unpacked_data = struct.unpack(struct_fmt, packed_data)
+    #Return unpacked data for tests
     return unpacked_data
 
+#Basic Test For Packing and Unpacking
+#Packed test data
 x = pack("schema3.txt",5,151,8,300,2,25,10,45,49,10,55)
+#Print testing data for test check
+data = (5,151,8,300,2,25,10,45,49,10,55)
+print data
+#Print packed data to see what packed data looks like
 print x
-print unpack("schema3.txt", x)
+#Print each packed data out in hex to actually understand packed data
+for each in x:
+    print binascii.hexlify(each)
+#Print unpacked data to verify it is correct
+print unpack("schema3.txt",x)
--- File_Format_test.py
+++ File_Format_test.py
@@ -1,11 +1,12 @@
 def pack_format(file):
-    #Open file to read
+    #Open file for read only to avoid accidental writing
     file = open(file, 'r')
-    #Initialize packing format to empty string
+    #Initialize packing format to empty string so more string elements can be added to it
     struct_fmt = ''
-    #Loop through each line in passed file
+    #Loop through each line in the format file to be sure everything is packed properly
     for line in file:
         #Add correct format variable depending on structure from passed file
+        #Python packing formatting can be found on https://docs.python.org/2/library/struct.html
         if line == "int8\n" or line == "int8":
             struct_fmt = struct_fmt + 'b'
 
@@ -23,21 +24,25 @@ def pack_format(file):
 
         if line == "uint32\n" or line == "uint32":
             struct_fmt = struct_fmt + 'I'
-    #Close file
+    #Close the file to avoid errors
     file.close()
-    #Return packing format to test
+    #Return packing format to test/print
     return struct_fmt
 
-#Test for each type used in schema 3
+#Create a packing format with a file containing each variable type present and print out returned format to check
 print pack_format("test.txt")
-#Test call using schema3 from github struct
+#Create packing format using a text file based on schema3 found in the github-server repository
 pack_format = pack_format("schema3.txt")
 
-#x is hard coded format from original Data-Translator file
+#x is hard coded format from original Data-Translator file to test against generated code for same packing scheme
 x = 'H'+'H'+'B'+'I'+'B'+'H'+'H'+'I'+'h'+'H'+'H'
 #Print hard coded format and generated format for sanity check
 print pack_format
 print x
-#Compare both strings to be sure they are the same
+#Compare both strings
+#If the generated format matches the hard coded format, print True to the console
 if x == pack_format:
     print True
+#If the generated file does not match, print message to the console
+else:
+    print "Packing format does not match!"
